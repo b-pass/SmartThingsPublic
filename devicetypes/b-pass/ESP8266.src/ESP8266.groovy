a5e8e52e855997f0d711e57e01d5eee7c71e1318
b-pas <b-pass@users.noreply.github.com>
2016-05-24 19:53:34 -0400

    Initial check-in of some local work on an Arduino-based Lux sensor built on an ESP8266

---
+++ devicetypes/b-pass/ESP8266.src/ESP8266.groovy
@@ -0,0 +1,167 @@
+/*
+ESP8266 Device, talks to an ESP8266 running "smartthings_lux.ino"
+The device is Discovered/Added to SmartThings via the SmartApp device discovery at: https://github.com/b-pass/SmartThingsPublic/smartapps/b-pass/ESP8266-Discovery.src/ESP8266-Discovery.groovy
+*/
+metadata {
+	definition (name: "ESP8266 UPnP Device", 
+              namespace: "b-pass",
+              author: "B. Pass",
+              category: "My Apps") {
+		capability "Illuminance Measurement"
+		capability "Sensor"
+		capability "Refresh"
+	}
+
+	simulator {
+	}
+
+	tiles {
+		valueTile("illuminance", "device.illuminance", decoration: "flat") {
+			state "luminosity", label:'${currentValue} lux'
+		}
+    standardTile("refresh", "null", inactiveLabel: false, decoration: "flat") {
+      state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+    }
+		main "illuminance"
+		details "illuminance", "refresh"
+	}
+}
+
+def installed() {
+    log.trace "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.trace "Updated with settings: ${settings}"
+    //unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    log.trace "initialize"
+    doSubscribe()
+}
+
+def refresh() {
+  log.debug "Refreshing!"
+  doSubscribe
+}
+
+def updateValueNow() {
+  sendHubCommand(new physicalgraph.device.HubAction(
+      method: "GET",
+      path: "/lux",
+      headers: [
+          Host: getHostAddress()
+      ]
+  ))
+}
+
+def parse(String description) {
+  // NOTE: We could tell a response from a NOTIFY by checking json vs. xml or checking status code (200 vs. null) or checking for SEQ/NT/SID header
+  
+  def msg = parseLanMessage(description)
+  //log.debug "msg: ${msg}"
+  if (!msg)
+  {
+    log.warn "Failed to parse $description as LanMessage"
+    return
+  }
+  
+  if (!state.sid || (msg?.headers?.seq != null && msg.headers.seq?.toInteger() == 0))
+  {
+    log.debug "Updated subscription from ${state.sid} to ${msg?.headers?.sid}"
+    state.sid = msg?.headers?.sid
+  }
+  
+  if (state.sid != msg?.headers?.sid)
+  {
+    log.debug "Expected sid ${state.sid} doesn't match supplied sid ${msg?.headers?.sid}"
+    return
+  }
+  
+  log.debug "Got value ${msg?.xml} on sid ${state.sid}"
+  
+  //log.debug "json is ${msg?.json}"
+  //log.debug "xml is ${msg?.xml}"
+  
+  // status is null if it's an http request (such as NOTIFY)
+  if (msg.status && msg.status != 200)
+  {
+    log.warn "HTTP status code ${msg.status}"
+    return
+  }
+  
+  if (!msg?.xml)
+    return // nothing to do
+  
+  return [createEvent([
+    name:"illuminance",
+    value:msg.xml,
+    unit:"lux"
+  ])]
+}
+
+def sync(ip, port) {
+log.trace "sync ${ip}, ${port}"
+	def existingIp = getDataValue("ip")
+	def existingPort = getDataValue("port")
+	if (ip && ip != existingIp) {
+		updateDataValue("ip", ip)
+	}
+	if (port && port != existingPort) {
+		updateDataValue("port", port)
+	}
+}
+
+def doSubscribe(callbackPath="") {
+    log.trace "doSubscribe($callbackPath)"
+    
+    state.sid = null;
+    sendHubCommand(new physicalgraph.device.HubAction(
+        method: "SUBSCRIBE",
+        path: "/subscribe",
+        headers: [
+            Host: getHostAddress(),
+            CALLBACK: "<http://${getCallBackAddress()}/notify${callbackPath}>",
+            NT: "upnp:event",
+            TIMEOUT: "Second-3630"
+        ]
+    ))
+
+    log.trace "SUBSCRIBE $path"
+    runIn(3600, doSubscribe)
+}
+
+// gets the address of the hub
+def getCallBackAddress() {
+    return device.hub.getDataValue("localIP") + ":" + device.hub.getDataValue("localSrvPortTCP")
+}
+
+// gets the address of the device
+def getHostAddress() {
+    def ip = getDataValue("ip")
+    def port = getDataValue("port")
+
+    if (!ip || !port) {
+        def parts = device.deviceNetworkId.split(":")
+        if (parts.length == 2) {
+            ip = parts[0]
+            port = parts[1]
+        } else {
+            log.warn "Can't figure out ip and port for device: ${device.id}"
+        }
+    }
+
+    //log.debug "Using IP: $ip and port: $port for device: ${device.id}"
+    return convertHexToIP(ip) + ":" + convertHexToInt(port)
+}
+
+Integer convertHexToInt(hex) {
+    return Integer.parseInt(hex,16)
+}
+
+String convertHexToIP(hex) {
+    return [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+}
---
+++ smartapps/b-pass/ESP8266-Discovery.src/ESP8266-Discovery.groovy
@@ -0,0 +1,191 @@
+/**
+ESP8266 Discovery App, talks to an ESP8266 running: https://github.com/b-pass/SmartThingsPublic/smartapps/b-pass/ESP8266.src/smartthings_lux.ino
+Adds these type devices: https://github.com/b-pass/SmartThingsPublic/smartapps/b-pass/ESP8266.src/ESP8266.groovy
+*/
+definition(
+		name: "ESP8266 UPnP Service Manager",
+		namespace: "b-pass",
+		author: "B. Pass",
+		description: "Discovers ESP8266-based things",
+		category: "My Apps",
+		iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+		iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+		iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+	page(name: "deviceDiscovery", title: "UPnP Device Setup", content: "deviceDiscovery")
+}
+
+def deviceDiscovery() {
+	log.debug "inside deviceDiscovery"
+    
+	def options = [:]
+	def devices = getVerifiedDevices()
+	devices.each {
+		def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
+		def key = it.value.mac
+		options["${key}"] = value
+	}
+
+	ssdpSubscribe()
+	ssdpDiscover()
+	verifyDevices()
+
+	return dynamicPage(name: "deviceDiscovery", title: "Discovery Started!", nextPage: "", refreshInterval: 5, install: true, uninstall: true) {
+		section("Please wait while we discover your UPnP Device. Discovery can take five minutes or more, so sit back and relax! Select your device below once discovered.") {
+			input "selectedDevices", "enum", required: false, title: "Select Devices (${options.size() ?: 0} found)", multiple: true, options: options
+		}
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	log.debug "inside initialize"
+    
+	unsubscribe()
+	unschedule()
+
+	ssdpSubscribe()
+
+	if (selectedDevices) {
+		addDevices()
+	}
+
+	runEvery5Minutes("ssdpDiscover")
+}
+
+void ssdpDiscover() {
+	log.debug "inside ssdpDiscover"
+	sendHubCommand(new physicalgraph.device.HubAction("lan discovery urn:schemas-upnp-org:device:esp8266_huzzah", physicalgraph.device.Protocol.LAN))
+}
+
+void ssdpSubscribe() {
+	log.debug "inside ssdpSubscribe"
+	subscribe(location, "ssdpTerm.urn:schemas-upnp-org:device:esp8266_huzzah", ssdpHandler)
+}
+
+Map verifiedDevices() {
+	log.debug "inside verifiedDevices"
+	def devices = getVerifiedDevices()
+	def map = [:]
+	devices.each {
+		def value = it.value.name ?: "UPnP Device ${it.value.ssdpUSN.split(':')[1][-3..-1]}"
+		def key = it.value.mac
+		map["${key}"] = value
+	}
+	map
+}
+
+void verifyDevices() {
+	log.debug "inside verifyDevices"
+	def devices = getDevices().findAll { it?.value?.verified != true }
+	devices.each {
+		int port = convertHexToInt(it.value.deviceAddress)
+		String ip = convertHexToIP(it.value.networkAddress)
+		String host = "${ip}:${port}"
+        log.debug "GET ${it.value.ssdpPath} from ${host}"
+		sendHubCommand(new physicalgraph.device.HubAction("""GET ${it.value.ssdpPath} HTTP/1.1\r\nHOST: $host\r\n\r\n""", physicalgraph.device.Protocol.LAN, host, [callback: deviceDescriptionHandler]))
+	}
+}
+
+def getVerifiedDevices() {
+	log.debug "inside getVerifiedDevices"
+	getDevices().findAll{ it.value.verified == true }
+}
+
+def getDevices() {
+	log.debug "inside getDevices"
+	if (!state.devices) {
+		state.devices = [:]
+	}
+	state.devices
+}
+
+def addDevices() {
+	log.debug "inside addDevices"
+    
+	def devices = getDevices()
+
+	selectedDevices.each { dni ->
+		def selectedDevice = devices.find { it.value.mac == dni }
+		def d
+		if (selectedDevice) {
+			d = getChildDevices()?.find {
+				it.deviceNetworkId == selectedDevice.value.mac
+			}
+		}
+
+		if (!d) {
+			log.debug "Creating Device with dni: ${selectedDevice.value.mac}"
+			addChildDevice("pass_things", "ESP8266 UPnP Device", selectedDevice.value.mac, selectedDevice.value.hub, [
+				"label": selectedDevice.value.name ?: "UPnP Device",
+				"data": [
+					"mac": selectedDevice.value.mac,
+					"ip": selectedDevice.value.networkAddress,
+					"port": selectedDevice.value.deviceAddress
+				]
+			])
+		}
+	}
+}
+
+def ssdpHandler(evt) {
+	log.debug "inside ssdpHandler"
+    
+	def description = evt.description
+	def hub = evt?.hubId
+
+	def parsedEvent = parseLanMessage(description)
+	parsedEvent << ["hub":hub]
+
+	def devices = getDevices()
+	String ssdpUSN = parsedEvent.ssdpUSN.toString()
+	if (devices."${ssdpUSN}") {
+		def d = devices."${ssdpUSN}"
+		if (d.networkAddress != parsedEvent.networkAddress || d.deviceAddress != parsedEvent.deviceAddress) {
+			d.networkAddress = parsedEvent.networkAddress
+			d.deviceAddress = parsedEvent.deviceAddress
+			def child = getChildDevice(parsedEvent.mac)
+			if (child) {
+				child.sync(parsedEvent.networkAddress, parsedEvent.deviceAddress)
+			}
+		}
+	} else {
+		devices << ["${ssdpUSN}": parsedEvent]
+	}
+}
+
+void deviceDescriptionHandler(physicalgraph.device.HubResponse hubResponse) {
+	log.debug "inside deviceDescriptionHandler"
+    
+	def body = hubResponse.xml
+	def devices = getDevices()
+	def device = devices.find {
+    	it?.key?.contains(body?.device?.UDN?.text())
+    }
+	if (device) {
+        def name = body?.device?.friendlyName ?: "UPnP Device"
+        name = "${name} @${device?.value?.mac}"
+		device.value << [name: name, model:body?.device?.modelNumber?.text(), serialNumber:body?.device?.serialNumber?.text(), verified: true]
+	}
+}
+
+private Integer convertHexToInt(hex) {
+	Integer.parseInt(hex,16)
+}
+
+private String convertHexToIP(hex) {
+	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+}
